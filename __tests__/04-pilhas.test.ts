47e40474f729b383559dd36604afb190e99212c8
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-05 16:08:26 -0300

    test: preparando terreno para o cap. pilhas

---
+++ __tests__/04-pilhas.test.ts
@@ -0,0 +1,30 @@
+describe("04 - Pilhas", () => {
+   describe("Criação de uma biblioteca de estruturas de dados e algoritmos JavaScript", () => {
+      it.todo("Estrutura de dados de pilha");
+
+      describe("Criando uma classe Stack baseada em array", () => {
+         it.todo("Push de elementos na pilha");
+         it.todo("Pop de elementos da pilha");
+         it.todo("Dando uma espiada no elemento que está no topo da pilha");
+         it.todo("Verificando se a pilha está vazia");
+         it.todo("Limpando os elementos da pilha");
+         it.todo("Usando a classe Stack");
+      });
+
+      describe("Criando uma classe JavaScript Stack baseada em objeto", () => {
+         it.todo("Push de elementos na pilha");
+         it.todo("Verificando se a pilha está vazia e o seu tamanho");
+         it.todo("Pop de elementos da pilha");
+         it.todo("Dando uma espiada no topo e limpando a pilha");
+         it.todo("Criando o método toString");
+         it.todo("Protegendo os elementos internos da estrutura de dados");
+         it.todo("Converção de nomenclatura com underscore");
+         it.todo("Classes ES2015 com símbolos no escopo");
+         it.todo("Classes ES2015 com WeakMap");
+         it.todo("Proposta para campos de classe na ECMAScript");
+         it.todo("Resolvendo problemas usando pilhas");
+         it.todo("Convertendo números decimais para binários");
+         it.todo("Algoritmo conversor de base");
+      });
+   });
+});
