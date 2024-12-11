5bd15d633293b0a6066f44cfde49872688613328
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-11 17:14:36 -0300

    test: exercicios pilha

--- __tests__/04-pilhas/Exercicios.test.ts
+++ __tests__/04-pilhas/Exercicios.test.ts
@@ -1,4 +1,16 @@
+import utils from "../../src/04-pilhas/utils";
+
 describe("04 - Pilhas: Resolvendo problemas usando pilhas", () => {
-   it.todo("Convertendo números decimais para binários");
-   it.todo("Algoritmo conversor de base");
+   it("Convertendo números decimais para binários", () => {
+      expect(utils.decimalToBinary(233)).toBe("11101001");
+      expect(utils.decimalToBinary(10)).toBe("1010");
+      expect(utils.decimalToBinary(1000)).toBe("1111101000");
+   });
+
+   it("Algoritmo conversor de base", () => {
+      expect(utils.baseConverter(100345, 2)).toBe("11000011111111001");
+      expect(utils.baseConverter(100345, 8)).toBe("303771");
+      expect(utils.baseConverter(100345, 16)).toBe("187F9");
+      expect(utils.baseConverter(100345, 35)).toBe("2BX0");
+   });
 });
