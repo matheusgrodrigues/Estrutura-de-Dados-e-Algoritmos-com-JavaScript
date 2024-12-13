2d324aef91cd885679f4ea64713850d065ea428d
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-13 16:01:01 -0300

    test: Estrutura de dados de deque

--- __tests__/05-filas-e-deques/05-filas-e-deques.test.ts
+++ __tests__/05-filas-e-deques/05-filas-e-deques.test.ts
@@ -1,4 +1,5 @@
 import Queue from "../../src/05-filas-e-deques/Queue";
+import Deque from "../../src/05-filas-e-deques/Deque";
 
 describe("05 - Filas e Deques", () => {
    describe("Criando a classe Queue", () => {
@@ -47,7 +48,7 @@ describe("05 - Filas e Deques", () => {
          expect(queue.isEmpty()).toBeTruthy();
       });
 
-      it.only("Criando o método toString", () => {
+      it("Criando o método toString", () => {
          const queue = new Queue();
 
          queue.enqueue(1);
@@ -57,8 +58,83 @@ describe("05 - Filas e Deques", () => {
    });
 
    describe("Estrutuda de dados de Deque", () => {
-      it.todo("Criando a classe Deque");
-      it.todo("Adicionando elementos na frente do deque");
+      it("Criando a classe Deque", () => {
+         const deque = new Deque();
+
+         expect(deque).toBeInstanceOf(Deque);
+      });
+
+      it("Adicionando elementos na frente do deque", () => {
+         const deque = new Deque();
+
+         deque.addFront(1);
+
+         expect(deque.peekFront()).toBe(1);
+      });
+
+      it("Adiciona um novo elemento no fim do deque", () => {
+         const deque = new Deque();
+
+         deque.addBack(7);
+
+         expect(deque.peekBack()).toBe(7);
+      });
+
+      it("Remove o primeiro elemento do deque", () => {
+         const deque = new Deque();
+
+         deque.addFront(1);
+         deque.addFront(2);
+      });
+
+      it("Remove o último elemento do deque", () => {
+         const deque = new Deque();
+
+         deque.addBack(2);
+
+         expect(deque.peekBack()).toBe(2);
+      });
+
+      it("Devolve o primeiro elemento do deque", () => {
+         const deque = new Deque();
+
+         deque.addFront(2);
+
+         expect(deque.peekFront()).toBe(2);
+      });
+
+      it("Devolve o último elemento do deque", () => {
+         const deque = new Deque();
+
+         deque.addBack(7);
+
+         expect(deque.peekBack()).toBe(7);
+      });
+
+      it("Retorna o tamanho do deque", () => {
+         const deque = new Deque();
+
+         deque.addBack(1);
+         deque.addBack(2);
+         deque.addBack(3);
+
+         expect(deque.size()).toBe(3);
+      });
+
+      it("Retorna se o deque está vazio", () => {
+         const deque = new Deque();
+
+         expect(deque.isEmpty()).toBeTruthy();
+      });
+
+      it("Retorna o deque em formato string", () => {
+         const deque = new Deque();
+
+         deque.addBack("John");
+         deque.addBack("Jack");
+
+         expect(deque.toString()).toBe("John, Jack");
+      });
    });
 
    describe("Resolvendo problemas usando filas e deques", () => {
