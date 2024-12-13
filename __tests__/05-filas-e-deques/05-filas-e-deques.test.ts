20c4586dc237392fb2fd9de7a7c1a6cef5348c74
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-13 15:23:26 -0300

    test: Criando a classe Queue

--- __tests__/05-filas-e-deques/05-filas-e-deques.test.ts
+++ __tests__/05-filas-e-deques/05-filas-e-deques.test.ts
@@ -12,7 +12,7 @@ describe("05 - Filas e Deques", () => {
          expect(queue.items).toEqual({ 0: 1, 1: 2, 2: 3 });
       });
 
-      it.only("Remoção de elementos da fila", () => {
+      it("Remoção de elementos da fila", () => {
          const queue = new Queue();
 
          queue.enqueue(1);
@@ -24,10 +24,36 @@ describe("05 - Filas e Deques", () => {
          expect(queue.items).toEqual({ 1: 2, 2: 3 });
       });
 
-      it.todo("Dando uma espiada no elemento que está na frente da fila");
-      it.todo("Verificando se a pilha está vazia e o seu tamanho");
-      it.todo("Limpando a fila");
-      it.todo("Criando o método toString");
+      it("Dando uma espiada no elemento que está na frente da fila", () => {
+         const queue = new Queue();
+
+         queue.enqueue(1);
+
+         expect(queue.peek()).toBe(1);
+      });
+
+      it("Verificando se a pilha está vazia e o seu tamanho", () => {
+         const queue = new Queue();
+
+         expect(queue.isEmpty()).toBeTruthy();
+         expect(queue.size()).toBe(0);
+      });
+
+      it("Limpando a fila", () => {
+         const queue = new Queue();
+
+         queue.clear();
+
+         expect(queue.isEmpty()).toBeTruthy();
+      });
+
+      it.only("Criando o método toString", () => {
+         const queue = new Queue();
+
+         queue.enqueue(1);
+
+         expect(queue.toString()).toBe("1");
+      });
    });
 
    describe("Estrutuda de dados de Deque", () => {
